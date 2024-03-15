efe7673368289092767050b02a44809bcdd01c9d
OscarFuentes2027 <drackelive@gmail.com>
2024-03-15 12:01:36 -0600

    getTag listuwu

--- items/item.router.js
+++ items/item.router.js
@@ -1,8 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const Item = require('../database/models.js').Item;
+
+
+router.use((req, res, next) => {
+  res.setHeader('Content-Security-Policy', 'default-src \'self\'; img-src *');
+  next();
+});
 
 router.get('/', (req, res) => {
-    res.send('Esta es una ruta de ejemplo item.');
-  });
+  res.send('Esta es una ruta de ejemplo item.');
+});
+
+router.get('/tag', (req, res) => {
   
-  module.exports = router;
+  Item.find({}, 'tag')
+    .then(tags => {
+      res.send(tags);
+      console.log(tags);
+    })
+    .catch(err => {
+      console.error('Error al consultar tags:', err);
+    });
+});
+
+module.exports = router;
