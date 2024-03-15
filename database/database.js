550f86326f4c0621f935b61fe5dad80c1f79a9b2
OscarFuentes2027 <drackelive@gmail.com>
2024-03-15 11:34:53 -0600

    nose

--- database/database.js
+++ database/database.js
@@ -1,11 +1,7 @@
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-dotenv.config();
-const password = process.env.DB_PASS;
+const mongoose = require('mongoose');
+const password = 'aho3kzQWUieojGzc';
 const dbName = 'ButzuBD'
-const dbUser = process.env.DB_USER;
 
-mongoose.connect(`mongodb+srv://${dbUser}:${password}@butzu-mobile.jsj2xpf.mongodb.net/${dbName}?retryWrites=true&w=majority&appName=Butzu-mobile`)
+mongoose.connect(`mongodb+srv://ButzuAdmin:${password}@butzu-mobile.jsj2xpf.mongodb.net/${dbName}?retryWrites=true&w=majority&appName=Butzu-mobile`)
   .then(() => console.log('Conexión a MongoDB exitosa en la base de datos:', dbName))
   .catch((err) => console.error('Error al conectar a MongoDB', err));
-
